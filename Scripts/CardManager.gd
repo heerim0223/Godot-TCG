484e3865a564b707338d80cfe7f518f6d7512ea1
멘헤라개발자 <74406700+heerim0223@users.noreply.github.com>
2026-07-16 11:34:05 +0900

    init: first commit

---
+++ Scripts/CardManager.gd
@@ -0,0 +1,151 @@
+extends Node2D
+
+
+const COLLISION_MASK_CARD = 1
+const COLLISION_MASK_CARD_SLOT = 2
+const SNAP_DISTANCE = 80.0
+
+var screen_size
+var card_being_dragged
+var is_hovering_on_card
+var player_hand_reference
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	#print(get_tree().get_nodes_in_group("card_slots"))
+	player_hand_reference = $"../PlayerHand"
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if card_being_dragged:
+		var mouse_pos = get_global_mouse_position()
+		card_being_dragged.global_position = Vector2(
+												clamp(mouse_pos.x, 0, screen_size.x), 
+												clamp(mouse_pos.y, 0, screen_size.y)
+											)
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			var card = raycast_check_for_card()
+			if card:
+				start_drag(card)
+		else:
+			if card_being_dragged:
+				finish_drag()
+
+
+func start_drag(card):
+	if card.current_slot:
+		card.current_slot.card_in_slot = false
+		card.current_slot = null
+
+	card_being_dragged = card
+	card.scale = Vector2(5, 5)
+	
+
+func finish_drag():
+	card_being_dragged.scale = Vector2(5.5, 5.5)
+
+	var card_slot_found = get_nearest_card_slot()
+
+	if card_slot_found:
+		var tween = create_tween()
+		tween.tween_property(
+			card_being_dragged,
+			"global_position",
+			card_slot_found.global_position,
+			0.15
+		)
+
+		player_hand_reference.remove_card_from_hand(card_being_dragged)
+		
+		card_slot_found.card_in_slot = true
+		card_being_dragged.current_slot = card_slot_found
+	else:
+		player_hand_reference.add_card_to_hand(card_being_dragged)
+
+	card_being_dragged = null
+
+
+func connect_card_signals(card):
+	card.connect("hovered", on_hovered_over_card)
+	card.connect("hovered_off", on_hovered_off_card)
+
+
+func on_hovered_over_card(card):
+	if !is_hovering_on_card:
+		is_hovering_on_card = true
+		highlight_card(card, true)
+
+	
+func on_hovered_off_card(card):
+	if !card_being_dragged:
+		# if not dragging
+		highlight_card(card, false)
+		
+		# Check if hovered off card straight on to another card
+		var new_card_hovered = raycast_check_for_card()
+		
+		if new_card_hovered:
+			highlight_card(new_card_hovered, true)
+		else:
+			is_hovering_on_card = false
+	
+
+func highlight_card(card, hovered):
+	if hovered:
+		card.scale = Vector2(5.3, 5.3)
+		card.z_index = 2
+	else:
+		card.scale = Vector2(5, 5)
+		card.z_index = 1
+		
+
+
+func get_nearest_card_slot():
+	var nearest_slot = null
+	var nearest_distance = SNAP_DISTANCE
+
+	for slot in get_tree().get_nodes_in_group("card_slots"):
+		if slot.card_in_slot:
+			continue
+
+		var distance = card_being_dragged.global_position.distance_to(slot.global_position)
+
+		if distance < nearest_distance:
+			nearest_distance = distance
+			nearest_slot = slot
+
+	return nearest_slot
+
+
+func raycast_check_for_card():
+	var space_state = get_world_2d().direct_space_state
+	var parameters = PhysicsPointQueryParameters2D.new()
+	parameters.position = get_global_mouse_position()
+	parameters.collide_with_areas = true
+	parameters.collision_mask = COLLISION_MASK_CARD
+	var result = space_state.intersect_point(parameters)
+	if result.size() > 0:
+		#return result[0].collider.get_parent()
+		return get_card_with_highest_z_index(result)
+	return null
+
+
+func get_card_with_highest_z_index(cards):
+	# Assume the first card in cards array has the highest z index
+	var highest_z_card = cards[0].collider.get_parent()
+	var highest_z_index = highest_z_card.z_index
+	
+	# Loop through the rest of the cards checking for a higher z index
+	for i in range(1, cards.size()):
+		var current_card = cards[i].collider.get_parent()
+		if current_card.z_index > highest_z_index:
+			highest_z_card = current_card
+			highest_z_index = current_card.z_index
+	return highest_z_card
